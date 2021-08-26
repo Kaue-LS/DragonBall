cff658612f528db316ce681e8c7d1d0483b51011
Kaue-LS <kaue.LS0506@gmail.com>
2021-08-26 20:24:02 -0300

    Update styled.js

--- dragonball/src/Components/Title/styled.js
+++ dragonball/src/Components/Title/styled.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Title = styled.h2`
   padding: 1rem 0 0rem 0;
-  font-size:3rem;
+  max-width:100%;
+  font-size:2.5rem;
   color: #800000;
   text-align: center;
 `;
